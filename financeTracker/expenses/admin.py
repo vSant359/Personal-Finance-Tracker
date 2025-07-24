3816f2b27174d2917f6d5e33e27a96cc91c0e417
vSant359 <vitorsantanaf359@gmail.com>
2025-07-24 15:59:11 -0300

    Created DefaultExpense model and made structural updates for future financial calculations

--- financeTracker/expenses/admin.py
+++ financeTracker/expenses/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, DefaultExpense
+
+""" This option is not necessary in this moment, but i think i can use this in the future:
+
+@admin.register(ExpenseType)
+class ExpenseTypeAdmin(admin.ModelAdmin):
+    pass
+    
+"""
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
@@ -8,3 +16,9 @@ class ExpenseAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     ordering = ('-date',)
 
+@admin.register(DefaultExpense)
+class DefaultExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'description', 'active')
+    list_filter = ('name', 'active')
+    search_fields = ('title', 'description')
+    
--- financeTracker/expenses/models.py
+++ financeTracker/expenses/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
+""" This option is not necessary in this moment, but i think i can use this in the future:
+
+class ExpenseType(models.Model):
+    name = models.CharField(max_length=100)
+    default_value = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+"""
+class DefaultExpense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
 class Expense(models.Model):
     CATEGORIES = [
         ('food', 'Food'),
@@ -12,11 +32,17 @@ class Expense(models.Model):
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORIES)
+    from_template = models.ForeignKey(DefaultExpense, null=True, blank=True, on_delete=models.SET_NULL) #This is not the way i picture it, but i did in this way for tests, it will be a checkbox.
     date = models.DateField()
     description = models.TextField(blank=True)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title} - R$ {self.amount}"
+
+
+
+
+
 
 # Create your models here.
